54eb771549c8872a0e7bdde67918b2663f54b02d
ibn-misbah <kawnuma@gmail.com>
2026-04-01 21:53:49 +0300

    feat: Express app instance with auth route configured

---
+++ server/src/app.ts
@@ -0,0 +1,73 @@
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { Request, Response } from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+
+import authRoutes from "./router/auth.router";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const API_VERSION = process.env.API_VERSION || "v1";
+const API_PREFIX = `/api/${API_VERSION}`;
+
+const isProduction =
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "PRODUCTION";
+const vpsURL = process.env.VPS_URL;
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const BASE_URL = isProduction ? vpsURL : "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: BASE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    status: 429,
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+app.use(limiter);
+app.use(cookieParser());
+
+// Routes
+app.use(`${API_PREFIX}/auth`, authRoutes);
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Health check passed",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+// -------404 Handler-------------
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.originalUrl} not found`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${isProduction ? "PRODUCTION" : "DEVELOPMENT"} mode`,
+  );
+  console.log(`http://localhost:${PORT}/`);
+});
