b233feabc42916a81a774c0395e48f06c097d900
ibn-misbah <kawnuma@gmail.com>
2026-04-20 22:56:36 +0300

    feat: added socket provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { router } from "./router";
+import { SocketProvider } from "./contexts/SocketContext";
 
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <SocketProvider>
+        <RouterProvider router={router} />
+      </SocketProvider>
     </AuthProvider>
   );
 }
