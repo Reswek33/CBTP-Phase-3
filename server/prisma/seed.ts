0a0852e9b404f23efdd6b4f70ba62d3225aeebbc
ibn-misbah <kawnuma@gmail.com>
2026-04-24 10:33:33 +0300

    seed script

---
+++ server/prisma/seed.ts
@@ -0,0 +1,44 @@
+import bcrypt from "bcryptjs";
+import "dotenv/config";
+
+import { PrismaClient } from "./../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash("1234567890", salt);
+
+  console.log("--- Cleaning database ---");
+  // Order matters due to foreign key constraints
+  await prisma.bid.deleteMany();
+  await prisma.rfp.deleteMany();
+  await prisma.supplier.deleteMany();
+  await prisma.buyer.deleteMany();
+  await prisma.user.deleteMany();
+
+  console.log("--- Seeding Users and Profiles ---");
+
+  await prisma.user.create({
+    data: {
+      firstName: "System",
+      lastName: "SUPERADMIN",
+      username: "superadmin",
+      email: "admin@system.com",
+      passwordHash: passwordHash,
+      role: "SUPERADMIN",
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
