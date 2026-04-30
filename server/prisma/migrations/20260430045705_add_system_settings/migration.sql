baca05f0de9e768836f6ac6f20825d5c564ee451
ibn-misbah <kawnuma@gmail.com>
2026-04-30 10:35:19 +0300

    dispute and system setting models

---
+++ server/prisma/migrations/20260430045705_add_system_settings/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "system_settings" (
+    "id" UUID NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "category" TEXT NOT NULL DEFAULT 'GENERAL',
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "system_settings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "system_settings_key_key" ON "system_settings"("key");
